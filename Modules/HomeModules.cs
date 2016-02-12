6b8a25778daab5716f8cf0c6435afbe1b532172f
alexL <alexL>
2016-02-12 08:03:32 -0800

    initial file commit

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -9,23 +9,9 @@ namespace todoNamespace
   {
     public HomeModule()
     {
-      Get["/"] =_=> {
-        return View ["add_view.cshtml"];
-      };
-      Get["/task/{id}"] = parameters => {
-        Task newTask = Task.Find(parameters.id);
-        return View["/task.cshtml", newTask];
-      };
-      Post["/view_all"] =_=> {
-        Task newTask = new Task(Request.Form["add"]);
-        // newTask.Save();
-        List<Task> _superlist = Task.GetAll();
-        return View ["view_all.cshtml", _superlist];
-      };
-      Get["/view_all_posts"] =_=> {
-        List<Task> _superlist = Task.GetAll();
-        return View ["view_all_posts.cshtml", _superlist];
-      };
+
+
+
     }
   }
 }
--- Objects/Tasks.cs
+++ Objects/Tasks.cs
@@ -5,35 +5,6 @@ namespace Todo.Objects
 {
   public class Task
   {
-      private string _taskMaster;
-      private int _id;
-      private static List<Task> _superlist = new List<Task> {};
-      /////Constructor function
-      public Task (string taskMaster)
-      {
-        _taskMaster = taskMaster;
-        _superlist.Add(this);
-        _id = _superlist.Count;
-
-      }
-      //// Get taskmaster function
-      public string GettaskMaster()
-      {
-        return _taskMaster;
-      }
-      // Get Taskmaster ID function
-      public int GetId()
-      {
-        return _id;
-      }
-      ////List All function
-      public static List<Task> GetAll()
-      {
-        return _superlist;
-      }
-      public static Task Find(int searchId)
-      {
-        return _superlist[searchId-1];
-      }
+    
   }
 }
